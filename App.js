e2266e6080e6090057534f6c0d0e23f94a437d62
jasmeet2910 <jasmeettuteja1993@gmail.com>
2023-01-14 09:16:27 +0530

    Chapter 03: Laying the foundation

--- App.js
+++ App.js
@@ -1,62 +1,38 @@
-// =========================================================================================
-//     const heading = React.createElement(
-//         "h1",
-//          {
-//         id:"title",
-//     },
-//      "Display Heading using react")
-//     console.log(heading);
-//     const root = ReactDOM.createRoot(document.getElementById("root"));
-//     const root1 = ReactDOM.createRoot(document.getElementById("footer"));
-//     const root2 = ReactDOM.createRoot(document.getElementById("header"));
-//     root.render(heading); // render function modifies our dom. so basically we are injecting h1 element to our dom where div id is root.
-//   root1.render(heading)
-//   root2.render(heading)
-
-//   // we can have multiple render and one root but in genrall we laways have one root and one render.
-
-// ======================================================================================
-// lets create complex html structire using react
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createlement => object => HTML => shown to DOM.
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "Heading 1"
-);
-
-//JSX => react.createlemt => obj => html => shown to UI.
-const heading3 = (
+const heading = (
   <h1 id="title" key="h1">
     Heading1
   </h1>
 );
 
-console.log("heading", heading);
-const heading1 = React.createElement(
-  "h2",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "heading 2"
+const Title = () => (
+  <h1 id="title" key="h1">
+    Heading1
+  </h1>
 );
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading1]
-);
+// React Component
+// functional  - a normal function which returns JSX i.e return some react elements.
+// Name of teh componenets should start with capital letters. Not manadatory.Just a convention.
 
-console.log("container", container);
-
+const HeaderComponent = () => {
+  return (
+    <div>
+      {
+        heading
+        // this heading variable will be replaced with above JSX expression
+      }
+      <Title />
+      {
+        // or we can call as
+        Title()
+      }
+      <h1> Namaste React Functional Component</h1>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent />);
